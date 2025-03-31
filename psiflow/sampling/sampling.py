fix units

psiflow/sampling/sampling.py
@@ -241,7 +241,8 @@ def setup_system_template(
         velocities.text = " 300 "
     if walkers[0].masses is not None:
         masses = ET.Element("masses", mode="manual")
-        masses.text = str(walkers[0].masses).replace("[", "[ ").replace("]", " ]")
+        amu = 1822.8885  # amu in atomic units
+        masses.text = str(list(walkers[0].masses * amu)).replace("[", "[ ").replace("]", " ]")  # replace str(list()) with np.array_str()?
         initialize.append(masses)
     initialize.append(velocities)
 

psiflow/sampling/walker.py
@@ -49,7 +49,7 @@ class Walker:
     state: Union[Geometry, AppFuture, None]
     temperature: Optional[float]
     pressure: Optional[float]
-    masses: Union[list, float, None] = None
+    masses: Union[np.ndarray, float, None] = None
     nbeads: int
     timestep: float
     coupling: Optional[Coupling]
@@ -63,7 +63,7 @@ class Walker:
         state: Union[Geometry, AppFuture, None] = None,
         temperature: Optional[float] = 300,
         pressure: Optional[float] = None,
-        masses: Union[list, float, None] = None,
+        masses: Union[np.ndarray, float, None] = None,
         nbeads: int = 1,
         timestep: float = 0.5,
         metadynamics: Optional[Metadynamics] = None,
@@ -83,8 +83,8 @@ class Walker:
         self.temperature = temperature
         self.pressure = pressure
 
-        if isinstance(masses, float):
-            masses = list(masses * self.start.atomic_masses)
+        if isinstance(masses, (float, int)):
+            masses *= self.start.atomic_masses
         self.masses = masses
         if self.masses is not None:
             assert len(self.masses) == len(self.start), "Masses do not match number of atoms"